7f029e6a46a4b579b59d30f619ede38d21844b8a
fernas <fernas@gmail.com>
2022-08-05 13:28:53 -0500

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <Counter :start="101" /> -->
+  <Indecision></Indecision>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: { 
+      // Counter,
+      Indecision
+    }
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,66 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div class="buttons-container">
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titulo: String,
+        start: {
+            type: Number,
+            default: 100,
+            validator(value) {
+                return value >= 0
+            }
+        }
+    },
+    name: 'Counter',
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+
+    methods: {
+        increase() {
+            this.counter++
+        },
+        decrease() {
+            this.counter--
+        }
+    },
+
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
+        },
+        customTitle() {
+            return this.titulo || 'Counter'
+        }
+    }
+}
+</script>
+
+<style>
+    button {
+        background-color: #64BB87;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+
+    button:hover {
+        background-color: #5AA67B;
+        transition: 0.3s ease-in-out;
+    }
+</style>
